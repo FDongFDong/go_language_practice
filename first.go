90f39c1ebf568ce9c5a526ed52fc5b76120f1ddc
FdongFdong <abnormal5626@naver.com>
2022-09-29 22:24:30 +0900

    반복문

--- first.go
+++ first.go
@@ -2,18 +2,29 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-func pow(x, n, lim float64) float64 {
-	if v := math.Pow(x, n); v < lim {
-		return v
-	} else {
-		fmt.Printf("%g >= %g \n", v, lim)
+func printhello(x int) int {
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			fmt.Println("Hello World")
+		}
 	}
-	return lim
+	return x
 }
 
 func main() {
-	fmt.Println(pow(3, 2, 10), pow(3, 3, 20))
+	sum := 0
+
+	// 반복문 1
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+
+	// 반복문 2
+	for sum < 1000 {
+		sum += 1
+	}
+
 }
