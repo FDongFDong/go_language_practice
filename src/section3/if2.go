3f49fec9b217b33d57e04871c70022cdab30818f
FDongFDong <abnormal5626@naver.com>
2022-11-29 16:21:01 +0900

    conditional study

---
+++ src/section3/if2.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 50
+	b := 70
+
+	// 예제 1
+	if a >= 65 {
+		fmt.Println("65 이상")
+	} else {
+		fmt.Println("65 미만")
+	}
+
+	if b >= 70 {
+		fmt.Println("70 이상")
+	} else {
+		fmt.Println("70 미만")
+	}
+
+	// 에러 발생
+	/* 괄호 위치
+	if a >= 65 {
+		fmt.Println("65 이상")
+	} else
+	{
+		fmt.Println("65 이하")
+	}
+	*/
+}
